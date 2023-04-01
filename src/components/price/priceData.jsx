3744c3fa40b250d86dfcfd7d632de34cdba6ce19
innoxee <innoxeee@gmail.com>
2023-04-01 22:23:03 +0500

    4

--- src/components/price/priceData.jsx
+++ src/components/price/priceData.jsx
@@ -97,10 +97,10 @@ const PriceData = () => {
      <Swiper
       breakpoints={{
         420: {
-          slidesPerView: 1.2,
+          slidesPerView: 1.1,
         },
         460: {
-          slidesPerView: 1.25,
+          slidesPerView: 1.2,
         },
         580: {
           slidesPerView: 1.4,
@@ -128,7 +128,7 @@ const PriceData = () => {
         }
       }}
       spaceBetween={1}
-      slidesPerView={1.2}
+      slidesPerView={1.1}
      >
       {cardData.map((card, index) => (
         <SwiperSlide>
